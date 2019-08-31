97690d4f50f0d629a24e45f43416db0c3a3f72c6
mk-nazemi <nazemi.works@gmail.com>
2019-08-31 12:22:32 +0430

    add SafaInput component +  parent comp in Sampleforcomponent folder for implementing test pattern

---
+++ src/components/SafaInput.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="safa-input">
+    <!-- readonly counter -->
+    <div class="label-container">
+      <label v-if="aligned" :for="idOfInput" class="label">{{ label }}</label>
+      <label v-else :for="idOfInput" class="left-label">{{ label }}</label>
+    </div>
+    <div class="input-container">
+      <q-input
+        :readonly="read"
+        :disable="notEditable"
+        ref="input"
+        autogrow
+        lazy-rules
+        outlined
+        bottom-slots
+        standout="bg-lime-1 text-{color}"
+        counter
+        maxlength="65"
+        v-model="text"
+        :id="idOfInput"
+        :value="values"
+        :type="type"
+        :input-style="{
+        textAlign: right,
+        minHeight: '20px',
+        height: '25px' ,
+        padding: padding,
+        display: flex,
+        alignItems: center ,
+        justifyContent: center
+      }"
+        :input-class="{ 'label-aligned': aligned }"
+        :color="color"
+        :dense="dense"
+        :hint="helper"
+        :rules="[
+        val => !!val  || errorlabel ,
+        val => val.length >= 3 || 'حداقل 3 کاراکتر'
+      ]"
+      >
+        <template v-if="aligned" v-slot:prepend>
+          <q-icon
+            v-if="text && !read && !notEditable"
+            round
+            dense
+            flat
+            class="cursor-pointer"
+            name="cancel"
+            icon="cancel"
+            @click.stop="text = null"
+            @click.prevent="reset"
+          />
+        </template>
+        <template v-else v-slot:append>
+          <q-icon
+            v-if="text && !read && !notEditable"
+            round
+            dense
+            flat
+            class="cursor-pointer"
+            name="cancel"
+            icon="cancel"
+            @click.stop="text = null"
+            @click.prevent="reset"
+          />
+        </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import uid from "uuid/v4";
+export default {
+  name: "SafaInput",
+  data() {
+    return {
+      read: null,
+      notEditable: null,
+      aligned: null,
+      idOfInput: null,
+      text: "",
+      dense: true
+    };
+  },
+  props: {
+    align: {
+      type: String,
+      required: true,
+      default: "right",
+      validator: v => ["right", "left"].includes(v)
+    },
+
+    m: {
+      // read write
+      type: String,
+      validator: v => ["r", "e", "ne"].includes(v),
+      default: "e"
+    },
+    type: {
+      type: String,
+      default: "text",
+      required: true
+    },
+    value: { type: String, default: "" },
+    label: { default: "", type: String },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    // rahnameee
+    helper: { type: String, default: "شما اینجایی" },
+    errorlabel: { type: String, default: "تصحیح شود" },
+    padding: {
+      type: Number,
+      default: 7
+    },
+    iconsize: { type: String, default: "24px" },
+    icon: { type: String, default: "add" },
+    color: {
+      type: String,
+      default: "blue"
+    },
+    c: {
+      // label
+      type: String,
+      validator: v =>
+        [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "auto"
+        ].includes(v),
+      default: "4"
+    }
+  },
+  created() {
+    this.$set(this, "text", this.value);
+    const generatedId = "Safa" + "_" + uid();
+    this.$set(this, "idOfInput", generatedId);
+    this.align === "right"
+      ? this.$set(this, "aligned", true)
+      : this.$set(this, "aligned", false);
+    switch (this.m) {
+      case "r":
+        return this.$set(this, "read", true);
+      case "ne":
+        return this.$set(this, "notEditable", true);
+      default:
+        return;
+    }
+  },
+  methods: {
+    reset() {
+      this.$refs.input.resetValidation();
+    }
+  },
+  watch: {
+    idOfInput: "idGenerator"
+  }
+};
+</script>
+
+<style lang='scss'>
+@import url("http://cdn.font-store.ir/behdad.css");
+.safa-input {
+  font-family: "behdad", "Courier New", Courier, monospace;
+  padding: 0;
+  margin: 0;
+  .label-container {
+    position: relative;
+    & .label {
+      position: absolute;
+      padding: 0 4px;
+      bottom: -2rem;
+      right: -2.5rem;
+    }
+    & .left-label {
+      position: absolute;
+      padding: 0 4px;
+      bottom: -2rem;
+      left: -2.5rem;
+      // background-color: blue;
+    }
+  }
+  .input-container {
+    .label-aligned {
+      text-align: right;
+    }
+    .q-field__control .relative-position {
+      // background-color: blue;
+      padding: 0 px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+</style>
---
+++ src/components/SampleForComponents/ParentOfSafaInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <SafaInput
+      :align="align"
+      :m="m"
+      :type="type"
+      :value="value"
+      :label="label"
+      :placeholder="placeholder"
+      :helper="helper"
+      :errorlabel="errorlabel"
+      :padding="padding"
+      :iconsize="iconsize"
+      :icon="icon"
+      :color="color"
+      :c="c"
+    />
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+import SafaInput from "../components/SafaInput";
+export default {
+  name: "PageIndex",
+  components: {
+    SafaInput
+  },
+  data() {
+    return {
+      align: "right",
+      m: "e",
+      id: "43434",
+      type: "text",
+      value: "ورود اطلاعات",
+      label: "نام",
+      placeholder: "ورود اطلاعات",
+      helper: "شما اینجایی",
+      errorlabel: " الزامی",
+      // not usefull
+      padding: 5,
+      // not usefull
+      iconsize: "24px",
+      // it must be base on quasar icon name
+      icon: "event",
+      color: "blue",
+      c: "5"
+    };
+  }
+};
+</script>
